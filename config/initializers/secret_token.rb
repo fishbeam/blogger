58e8d121565847fea3ab33d7158ad57db7d9b78b
Tim Douglas <neurotech@gmail.com>
2012-09-11 13:32:05 +1000

    First push to start repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'f18fdc8d61516d9a81d604792745a30f9bfb41943e2d85537f0676b0b0bce37e937655e4fe7d5faba843bc20e46390a46a82689295d578daff4ff9b068108f95'
